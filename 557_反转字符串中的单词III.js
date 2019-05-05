c211e23dd42989e885c78539c5cef66faca266d6
web_xl <597146220@qq.com>
2019-05-05 22:32:08 +0800

    557. 反转字符串中的单词 III split配合正则

--- 557_反转字符串中的单词III.js
+++ 557_反转字符串中的单词III.js
@@ -2,7 +2,7 @@
  * @Author: web_XL
  * @Date: 2019-05-05 21:14:07
  * @Last Modified by: web_XL
- * @Last Modified time: 2019-05-05 22:07:26
+ * @Last Modified time: 2019-05-05 22:31:33
  */
 // 557. 反转字符串中的单词 III
 // 给定一个字符串，你需要反转字符串中每个单词的字符顺序，同时仍保留空格和单词的初始顺序。
@@ -14,9 +14,20 @@
 
 
 // 自己的思路  通过split(' ')将每个单词分开 然后将每个单词翻转后 用join(' ')合并成一个方法
+// let reverseWords = (s) => {
+//   return s
+//     .split(' ')
+//     .map(item => {
+//       return item
+//         .split('')
+//         .reverse()
+//         .join('')
+//     }).join(' ')
+// };
+
 let reverseWords = (s) => {
   return s
-    .split(' ')
+    .split(/\s/g)
     .map(item => {
       return item
         .split('')
@@ -34,26 +45,26 @@ var reverseWords = function (s) {
 }
 
 // 不同的思路
-let reverseWords = (s) => {
-  let str = ''//存出结果
-  let begin = 0//单词开始的位置 
-  for (let i = 0; i < s.length; i++) {
-    if (s[i] === ' ') {
-      //当遇到' ' ，将前面的单词进行翻转
-      for (let j = i - 1; j >= begin; j--) {
-        str += s[j]
-      }
-      str += ' ' //增加间隔
-      begin = i + 1  //更新单词开始的位置
-    }
+// let reverseWords = (s) => {
+//   let str = ''//存出结果
+//   let begin = 0//单词开始的位置 
+//   for (let i = 0; i < s.length; i++) {
+//     if (s[i] === ' ') {
+//       //当遇到' ' ，将前面的单词进行翻转
+//       for (let j = i - 1; j >= begin; j--) {
+//         str += s[j]
+//       }
+//       str += ' ' //增加间隔
+//       begin = i + 1  //更新单词开始的位置
+//     }
 
-  }
-  // 将最后一个单词 翻转
-  for (let n = s.length - 1; n >= begin; n--) {
-    str += s[n]
-  }
-  return str
-}
+//   }
+//   // 将最后一个单词 翻转
+//   for (let n = s.length - 1; n >= begin; n--) {
+//     str += s[n]
+//   }
+//   return str
+// }
 
 console.log(reverseWords("Let's take LeetCode contest"))
 
